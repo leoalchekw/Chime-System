a29e3e628ff39863b5170d358a67b2c422a99da3
leoalchekw <weisberl@union.edu>
2025-03-04 16:31:35 -0500

    Update song_decoder.ino

--- chime_system/song_decoder.ino
+++ chime_system/song_decoder.ino
@@ -10,8 +10,76 @@ void decodeSong(String song) {
 }
 
 void decodeNote(String note) {
-  // giant switch statement
-  // 1. find appropriate pin based off of note and octave (switch)
-  // 2. call appropriate function based off of note length char (switch)
-  // using pin found in 1. as parameter
+  char noteLength = note.charAt(0);
+  String noteName = note.substring(1, note.length);
+
+  int chimePin;
+
+  switch noteName
+  {
+    case "A(0)":
+      chimePin = A0_nat;
+      break;
+    case "A#(0)":
+      chimePin = A0_sharp;
+      break;
+    case "B(0)":
+      chimePin = B0_nat;
+      break;
+    case "C(0)":
+      chimePin  = C0_nat;
+      break;
+    case "C#(0)":
+      chimePin  = C0_sharp;
+      break;
+    case "D(0)":
+      chimePin  = D0_nat;
+      break;
+    case "D#(0)":
+      chimePin  = D0_sharp;
+      break;
+    case "E(0)":
+      chimePin  = E0_nat;
+      break;
+    case "F(0)":
+      chimePin  = F0_nat;
+      break;
+    case "F#(0)":
+      chimePin  = F0_sharp;
+      break;
+    case "G(0)":
+      chimePin  = G0_nat;
+      break;
+    case "G#(0)":
+      chimePin  = G0_sharp;
+      break;
+    case "A(1)":
+      chimePin = A1_nat;
+      break;
+    case "A#(1)"
+      chimePin = A1_sharp;
+      break;
+    default:
+      chimePin  = restPin;
+      break;
+  }
+
+  switch noteLength
+  {
+    case "s":
+      sixteenthNote(chimePin);
+      break;
+    case "e":
+      eighthNote(chimePine);
+      break;
+    case "q":
+      quarterNote(chimePin);
+      break;
+    case "h":
+      halfNote(chimePin);
+      break;
+    case "w":
+      wholeNote(chimePin);
+      break;
+  }
 }
