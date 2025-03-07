b278e700d9909ac819871b2b7c5827c676739154
leoalchekw <weisberl@union.edu>
2025-03-07 16:41:00 -0500

    Update song_decoder.ino

--- chime_system/song_decoder.ino
+++ chime_system/song_decoder.ino
@@ -1,85 +1,140 @@
 void decodeSong(String song) {
   // pass in a copy of the string
-  if (song.length > 0) {
-    int firstNoteEnd = copy.indexOf(' ');
-    String firstNote = copy.substring(0, firstNoteEnd);
+  if (song.length() > 0) {
+    int firstNoteEnd = song.indexOf(' ');
+    String firstNote = song.substring(0, firstNoteEnd);
     decodeNote(firstNote);
-    copy.remove(0, firstNoteEnd+1); // I think off by 1 error, so added 1
-    decodeSong(String copy);
+    song.remove(0, firstNoteEnd+1); // I think off by 1 error, so added 1
+    decodeSong(song);
   }  
 }
 
 void decodeNote(String note) {
   char noteLength = note.charAt(0);
-  String noteName = note.substring(1, note.length);
+  String noteName = note.substring(1, note.length());
 
   int chimePin;
 
-  switch noteName
-  {
-    case "A(0)":
-      chimePin = A0_nat;
-      break;
-    case "A#(0)":
-      chimePin = A0_sharp;
-      break;
-    case "B(0)":
-      chimePin = B0_nat;
-      break;
-    case "C(0)":
-      chimePin  = C0_nat;
-      break;
-    case "C#(0)":
-      chimePin  = C0_sharp;
-      break;
-    case "D(0)":
-      chimePin  = D0_nat;
-      break;
-    case "D#(0)":
-      chimePin  = D0_sharp;
-      break;
-    case "E(0)":
-      chimePin  = E0_nat;
-      break;
-    case "F(0)":
-      chimePin  = F0_nat;
-      break;
-    case "F#(0)":
-      chimePin  = F0_sharp;
-      break;
-    case "G(0)":
-      chimePin  = G0_nat;
-      break;
-    case "G#(0)":
-      chimePin  = G0_sharp;
-      break;
-    case "A(1)":
-      chimePin = A1_nat;
-      break;
-    case "A#(1)"
-      chimePin = A1_sharp;
-      break;
-    default:
+  if (noteName =  "A(0)")
+    {
+    chimePin = A0_nat;
+    }
+  else if (noteName =  "A#(0)")
+    {
+    chimePin = A0_sharp;
+    }
+  else if (noteName =  "B(0)")
+    {
+    chimePin = B0_nat;
+    }
+  else if (noteName =  "C(0)")
+    {
+    chimePin  = C0_nat;
+    }
+  else if (noteName =  "C#(0)")
+    {
+    chimePin  = C0_sharp;
+    }
+  else if (noteName =  "D(0)")
+    {
+    chimePin  = D0_nat;
+    }
+  else if (noteName =  "D#(0)")
+    {
+    chimePin  = D0_sharp;
+    }
+  else if (noteName =  "E(0)")
+    {
+    chimePin  = E0_nat;
+    }
+  else if (noteName =  "F(0)")
+    {
+    chimePin  = F0_nat;
+    }
+  else if (noteName =  "F#(0)")
+    {
+    chimePin  = F0_sharp;
+    }
+  else if (noteName =  "G(0)")
+    {
+    chimePin  = G0_nat;
+    }
+  else if (noteName =  "G#(0)")
+    {
+    chimePin  = G0_sharp;
+    }
+  else if (noteName =  "A(1)")
+    {
+    chimePin = A1_nat;
+    }
+  else if (noteName =  "A#(1)")
+    {
+    chimePin = A1_sharp;
+    }
+  else
+    {
       chimePin  = restPin;
-      break;
+    }
+
+  if (noteLength =  "s")    
+    {
+      sixteenthNote(chimePin);
+    }
+  else if (noteLength =  "e")
+    {
+      eighthNote(chimePin);
+    }
+  else if (noteName =  "q")
+    {
+      quarterNote(chimePin);
+    }
+  else if (noteName =  "h")
+    {
+      halfNote(chimePin);
+    }
+  else if (noteName =  "w")
+    {
+      wholeNote(chimePin);
+    }
+}
+
+// define functions for note length
+void sixteenthNote(int note)
+  {
+    digitalWrite(note, HIGH);
+    delay(188);
+    digitalWrite(note, LOW);
+    delay(62);
   }
 
-  switch noteLength
+void eighthNote(int note)
   {
-    case "s":
-      sixteenthNote(chimePin);
-      break;
-    case "e":
-      eighthNote(chimePine);
-      break;
-    case "q":
-      quarterNote(chimePin);
-      break;
-    case "h":
-      halfNote(chimePin);
-      break;
-    case "w":
-      wholeNote(chimePin);
-      break;
+    digitalWrite(note, HIGH);
+    delay(375);
+    digitalWrite(note, LOW);
+    delay(125);
   }
-}
+
+void quarterNote(int note)
+  {
+    digitalWrite(note, HIGH);
+    delay(750);
+    digitalWrite(note, LOW);
+    delay(250);
+  }
+
+void halfNote(int note)
+  {
+    digitalWrite(note, HIGH);
+    delay(1500);
+    digitalWrite(note, LOW);
+    delay(500);
+  }
+
+void wholeNote(int note)
+  {
+    digitalWrite(note, HIGH);
+    delay(3000);
+    digitalWrite(note, LOW);
+    delay(1000);
+  }
