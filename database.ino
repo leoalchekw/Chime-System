ac97e1eb543f2cc837481d3e03e09d5b3125faef
leoalchekw <weisberl@union.edu>
2025-03-13 22:48:18 -0400

    Final database.ino

--- database.ino
+++ database.ino
@@ -21,7 +21,7 @@
  * -----------------------------
  * w      | Whole note         |
  * -----------------------------
- * 0-1    | Octave of note     |
+ * 0-2    | Octave of note     |
  * -----------------------------
  * day    | Value 1-31         |
  * -----------------------------
@@ -32,21 +32,21 @@
 
 
 // Define notes used in songs
-String happyBdayNotes = String("eA(0) eA(0) qB(0) qA(0) qD(0) hC#(0) eA(0) eA(0) qB(0) qA(0) qE(0) hD(0) eA(0) eA(0) qA(1) qF#(0) qD(0) qC#(0) qB(0) eG(0) eG(0) qF#(0) qD(0) qE(0) wD(0) ");
-
+String happyBdayNotes = String("eA#(1) eA#(1) qC(1) qA#(1) qD#(1) hD(1) eA#(1) eA#(1) qC(1) qA#(1) qF(1) hD#(1) eA#(1) eA#(1) qA#(2) qG(1) qD#(1) qD(1) hC(1) eG#(1) eG#(1) qG(1) qD#(1) qF(1) wD#(1) ");
 
 //Define dates when songs should play (Month(1-12), Day(1-31), Hour(0-23), Minute(0-59))
-DateTime happyBdayWifeDate = createDate(12, 12, 18, 12);
-DateTime happyBdayDaughterDate = createDate(1, 1, 18, 1);
+DateTime happyBdayWifeDate = createDate(3, 10, 14, 12); // PLACEHOLDER DATE, PLEASE CHANGE
+DateTime happyBdayDaughterDate = createDate(1, 1, 18, 1); // PLACEHOLDER DATE, PLEASE CHANGE
 
+// Runs in loop to check if chimes should be played
 String scanSongs() {
-  if (compareDate(happyBdayWifeDate)) {
+  if (compareDate(happyBdayWifeDate)) { // if Wife birthday
     return happyBdayNotes;
   }
-  else if (compareDate(happyBdayDaughterDate)) {
+  else if (compareDate(happyBdayDaughterDate)) { // if Daughter birthday
     return happyBdayNotes;
   }
-  else {
-    return "";
+  else { // if no song is playing, chime the hour or half hour
+    return chimeHours();
   }
 }
