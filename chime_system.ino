e142c111587a00c5e4952529738aeaec1f597ef7
leoalchekw <weisberl@union.edu>
2025-03-13 22:47:32 -0400

    Final chime_system.ino

--- chime_system.ino
+++ chime_system.ino
@@ -1,75 +1,57 @@
 #include <RTClib.h>
 
 // Define which pins the chimes are attached too
-int A0_nat = 24;
-int A0_sharp = 26;
-int B0_nat = 28;
-int C0_nat = 30;
-int C0_sharp = 22;
-int D0_nat = 32;
-int D0_sharp = 39;
-int E0_nat = 36;
-int F0_nat = 38;
-int F0_sharp = 23;  //Possibly pin 39?
-int G0_nat = 25;  //Possibly pin 41?
-int G0_sharp = 27;  //Possibly pin 43?
-int A1_nat = 29;  //Possibly pin 45?
-int A1_sharp = 31;  ///Possibly pin 47?
+int A1_sharp = 24;
+int B1_nat = 26;
+int C1_nat = 28;
+int C1_sharp = 30;
+int D0_nat = 22;
+int D1_nat = 32;
+int D1_sharp = 34;
+int E1_nat = 36;
+int F1_nat = 38;
+int F1_sharp = 23;  
+int G1_nat = 25;  
+int G1_sharp = 27;  
+int A2_nat = 29;  
+int A2_sharp = 31;
 int restPin = 0;
 
-RTC_DS3231 rtc;
-DateTime now;
-String toPlay;
-
-void fullScale() {
-    sixteenthNote(A0_nat);
-    sixteenthNote(restPin);
-    eighthNote(B0_nat);
-    eighthNote(restPin);
-    quarterNote(C0_sharp);
-    quarterNote(restPin);
-    halfNote(D0_nat);
-    halfNote(restPin);
-    wholeNote(E0_nat);
-    wholeNote(restPin);
-    halfNote(F0_sharp);
-    halfNote(restPin);
-    quarterNote(G0_sharp);
-    quarterNote(restPin);
-    eighthNote(A1_nat);
-  }
+//Declare objects
+RTC_DS3231 rtc; // RTC object stored in rtc
+DateTime now; // current date and time stored in now
+String toPlay; // notes to be played stored in toPlay
   
 void setup() {
+
   // initialize the chime pins as outputs
-  pinMode(A0_nat, OUTPUT);
-  pinMode(A0_sharp, OUTPUT);
-  pinMode(B0_nat, OUTPUT);
-  pinMode(C0_nat, OUTPUT);
-  pinMode(C0_sharp, OUTPUT);
-  pinMode(D0_nat, OUTPUT);
-  pinMode(D0_sharp, OUTPUT);
-  pinMode(E0_nat, OUTPUT);
-  pinMode(F0_nat, OUTPUT);
-  pinMode(F0_sharp, OUTPUT);
-  pinMode(G0_nat, OUTPUT);
-  pinMode(G0_sharp, OUTPUT);
-  pinMode(A1_nat, OUTPUT);
   pinMode(A1_sharp, OUTPUT);
-  
+  pinMode(B1_nat, OUTPUT);
+  pinMode(C1_nat, OUTPUT);
+  pinMode(C1_sharp, OUTPUT);
+  pinMode(D0_nat, OUTPUT);
+  pinMode(D1_nat, OUTPUT);
+  pinMode(D1_sharp, OUTPUT);
+  pinMode(E1_nat, OUTPUT);
+  pinMode(F1_nat, OUTPUT);
+  pinMode(F1_sharp, OUTPUT);
+  pinMode(G1_nat, OUTPUT);
+  pinMode(G1_sharp, OUTPUT);
+  pinMode(A2_nat, OUTPUT);
+  pinMode(A2_sharp, OUTPUT);
+
+  // If a connection is established, begin RTC, otherwise try again
   if(!rtc.begin()) {
     delay(1000);
     setup();
-  }
+  } 
 }
 
-
-
 void loop() {
-  now = rtc.now();
-  toPlay = scanSongs();
-  if (!toPlay.equals("")) {
+  now = rtc.now(); // get current date and time
+  toPlay = scanSongs(); // check if chimes should play
+  if (!toPlay.equals("")) { // if no notes to be played, do not play
     decodeSong(toPlay);
-    delay(60000);
+    delay(60000); 
   }
-  
 }
